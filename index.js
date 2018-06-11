775fc2f205a4a52bdef4d7899a0c4ff5cdf6a40e
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-06-11 13:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// var katzDeliLine = [];
-
-// function takeANumber(katzDeliLine, person){
-  
-  
-// //     katzDeliLine.push(person)
-// //   return  `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`
-// // }
-
-
-
-// // function nowServing(katzDeliLine){
-// //   if(katzDeliLine.length < 1){
-// //     return "There is nobody waiting to be served!"
-// //   } else {
-// //     return `Currently serving ${katzDeliLine.shift()}.`
-// //   }
-// // }
-
-
-// // function 
-
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
@@ -37,36 +15,15 @@ function nowServing(line){
   }
 } 
 
-// function currentLine(line){
-//   if(line.length > 0){
-//     let nameAndLine = []
-//     for(let i = 0; i < line.length; i++){
-//       nameAndLine.push( ` ${i + 1}. ${line[i]}` )
-//     }
-//     return `The line is currently:${nameAndLine}`
-//   }
-//     return `The line is currently empty.`
-// }
 
-
-
-function currentLine(line) {
-  if (!line.length) {
-    return "The line is currently empty."
+function currentLine(line){
+  if(line.length > 0){
+    let nameAndLine = []
+    for(let i = 0; i < line.length; i++){
+      nameAndLine.push( ` ${i + 1}. ${line[i]}` )
+    }
+    return `The line is currently:${nameAndLine}`
   }
-
-  const numbersAndNames = []
-
-  for (let i = 0; i < line.length; i++) {
-    numbersAndNames.push(`${i + 1}. ${line[i]}`)
-  }
-
-  return `The line is currently: ${numbersAndNames.join(', ')}`
-};
-
-
-
-
-
-
+    return `The line is currently empty.`
+}
 
